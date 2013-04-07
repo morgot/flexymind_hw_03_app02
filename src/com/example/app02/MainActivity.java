69aa39107d44c1f712e70af386e0879d494c67b7
Gregory <morgot.gregory@gmail.com>
2013-04-07 16:08:22 +0400

    Готовое приложение

--- src/com/example/app02/MainActivity.java
+++ src/com/example/app02/MainActivity.java
@@ -1,22 +1,43 @@
 package com.example.app02;
 
-import android.os.Bundle;
+import java.util.Random;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-
-public class MainActivity extends Activity {
-
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+public class MainActivity extends Activity implements OnClickListener {
+	
+	Button btn;
+	Random rand = new Random();
+	RelativeLayout layout;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		layout = (RelativeLayout) findViewById(R.id.main);
+		btn = new Button(this);
+		btn.setText("ֽאזלט לוםÿ!");
+		
+		btn.setOnClickListener(this);
+		layout.addView(btn);
+		
 	}
 
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View v) {
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		layout = (RelativeLayout) findViewById(R.id.main);
+		
+		params.leftMargin = rand.nextInt(layout.getMeasuredWidth() - btn.getWidth() ) ;
+		params.topMargin = rand.nextInt(layout.getMeasuredHeight() - btn.getHeight() );
+		v.setLayoutParams(params);
+		
 	}
 
 }
